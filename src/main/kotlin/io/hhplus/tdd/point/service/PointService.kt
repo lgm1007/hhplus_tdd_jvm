0365a68a7b5b0a64c1638fc84b750693156c1231
lgm1007 <flxl654@gmail.com>
2024-09-24 21:07:57 +0900

    feat: 포인트 충전/사용 로직 시 동시성 제어를 ConcurrentHashMap, ReentrantLock 사용하도록 수정

--- src/main/kotlin/io/hhplus/tdd/point/service/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/service/PointService.kt
@@ -7,12 +7,18 @@ import io.hhplus.tdd.point.UserPoint
 import io.hhplus.tdd.point.dto.PointDto
 import io.hhplus.tdd.point.validate.PointValidator
 import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 @Service
 class PointService(
 	private val userPointTable: UserPointTable,
 	private val pointHistoryTable: PointHistoryTable,
 ) : PointUseCase {
+	// 동시성 제어 방식을 위한 Lock ConcurrentHashMap
+	// 같은 userId에 대한 요청은 Lock을 획득하여 대기하도록 하기 위함
+	private val userPointLocks = ConcurrentHashMap<Long, ReentrantLock>()
 
 	override fun getUserPointById(id: Long): UserPoint {
 		return userPointTable.selectById(id)
@@ -22,40 +28,50 @@ class PointService(
 		return pointHistoryTable.selectAllByUserId(userId)
 	}
 
-	// 동시성 제어를 위해 Synchronized method로 정의하여 하나의 스레드 씩 참조하도록 함
-	@Synchronized
 	override fun chargeUserPoint(pointDto: PointDto): UserPoint {
-		val point = userPointTable.selectById(pointDto.userId).point
-		val afterChargeAmount = point + pointDto.amount
+		// userId가 concurrentHashMap 에 없으면 새로운 lock 획득, 있으면 존재하는 lock 가져옴
+		val lock = userPointLocks.computeIfAbsent(pointDto.userId) {
+			ReentrantLock()
+		}
 
-		// 유저가 현재 보유한 포인트 + 충전하고자 하는 포인트가 최대 잔고를 넘는지 검사
-		PointValidator.validatePointOverMaxLimit(afterChargeAmount)
+		return lock.withLock {
+			val point = userPointTable.selectById(pointDto.userId).point
+			val afterChargeAmount = point + pointDto.amount
 
-		pointHistoryTable.insert(
-			pointDto.userId,
-			pointDto.amount,
-			pointDto.type,
-			System.currentTimeMillis()
-		)
+			// 유저가 현재 보유한 포인트 + 충전하고자 하는 포인트가 최대 잔고를 넘는지 검사
+			PointValidator.validatePointOverMaxLimit(afterChargeAmount)
 
-		return userPointTable.insertOrUpdate(pointDto.userId, afterChargeAmount)
+			pointHistoryTable.insert(
+				pointDto.userId,
+				pointDto.amount,
+				pointDto.type,
+				System.currentTimeMillis()
+			)
+
+			userPointTable.insertOrUpdate(pointDto.userId, afterChargeAmount)
+		}
 	}
 
-	@Synchronized
 	override fun useUserPoint(pointDto: PointDto): UserPoint {
-		val point = userPointTable.selectById(pointDto.userId).point
-		val afterUseAmount = point - pointDto.amount
+		val lock = userPointLocks.computeIfAbsent(pointDto.userId) {
+			ReentrantLock()
+		}
 
-		// 유저가 현재 보유한 포인트 - 사용하고자 하는 포인트가 최소 포인트 값보다 작은지 검사 (잔고 부족)
-		PointValidator.validatePointLackMinLimit(afterUseAmount)
+		return lock.withLock {
+			val point = userPointTable.selectById(pointDto.userId).point
+			val afterUseAmount = point - pointDto.amount
 
-		pointHistoryTable.insert(
-			pointDto.userId,
-			pointDto.amount,
-			pointDto.type,
-			System.currentTimeMillis()
-		)
+			// 유저가 현재 보유한 포인트 - 사용하고자 하는 포인트가 최소 포인트 값보다 작은지 검사 (잔고 부족)
+			PointValidator.validatePointLackMinLimit(afterUseAmount)
 
-		return userPointTable.insertOrUpdate(pointDto.userId, afterUseAmount)
+			pointHistoryTable.insert(
+				pointDto.userId,
+				pointDto.amount,
+				pointDto.type,
+				System.currentTimeMillis()
+			)
+
+			userPointTable.insertOrUpdate(pointDto.userId, afterUseAmount)
+		}
 	}
 }
