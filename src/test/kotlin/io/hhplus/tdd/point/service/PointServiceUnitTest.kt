3c1d18fa701825e903344c13c9ef44960a981094
lgm1007 <flxl654@gmail.com>
2024-09-24 22:21:42 +0900

    test: 포인트 사용에 대한 기능 및 예외케이스 테스트 + 내역 저장 기능 검증 테스트 추가

--- src/test/kotlin/io/hhplus/tdd/point/service/PointServiceUnitTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/service/PointServiceUnitTest.kt
@@ -34,12 +34,80 @@ class PointServiceUnitTest {
 	}
 
 	@Test
-	@DisplayName("포인트 충전 최대 잔고로 실패 예외케이스")
-	fun shouldChargePointExceptionTest() {
+	@DisplayName("포인트 충전에 대한 내역 저장 테스트")
+	fun insertChargeHistoryTest() {
+		val pointDtos = listOf(PointDto(1L, TransactionType.CHARGE, 100L), PointDto(1L, TransactionType.CHARGE, 200L))
+
+		pointDtos.forEach {
+			sut.chargeUserPoint(it)
+		}
+
+		val actual = sut.getAllPointHistoriesByUserId(1L)
+
+		assertAll(
+			{ assertThat(actual.size).isEqualTo(2) },
+			{ assertThat(actual[0].userId).isEqualTo(1L) },
+			{ assertThat(actual[0].type).isEqualTo(TransactionType.CHARGE) },
+			{ assertThat(actual[0].amount).isEqualTo(100L) },
+			{ assertThat(actual[1].userId).isEqualTo(1L) },
+			{ assertThat(actual[1].type).isEqualTo(TransactionType.CHARGE) },
+			{ assertThat(actual[1].amount).isEqualTo(200L) }
+		)
+	}
+
+	@Test
+	@DisplayName("포인트 충전 중 최대 잔고로 실패 예외케이스")
+	fun shouldExceptionChargePointLimitOverTest() {
 		val pointDto = PointDto(1L, TransactionType.CHARGE, 10_001L)
 
 		assertThrows<PointException> {
 			sut.chargeUserPoint(pointDto)
+		}
+	}
+
+	@Test
+	@DisplayName("포인트 사용 기능 테스트")
+	fun useUserPointTest() {
+		val chargePointDto = PointDto(1L, TransactionType.CHARGE, 200L)
+		val usePointDto = PointDto(1L, TransactionType.USE, 100L)
+
+		sut.chargeUserPoint(chargePointDto)
+		sut.useUserPoint(usePointDto)
+
+		val actual = sut.getUserPointById(1L)
+
+		assertAll(
+			{ assertThat(actual.id).isEqualTo(1L) },
+			{ assertThat(actual.point).isEqualTo(100L) },
+		)
+	}
+
+	@Test
+	@DisplayName("포인트 사용에 대한 내역 저장 테스트")
+	fun insertUseHistoryTest() {
+		val chargePointDto = PointDto(1L, TransactionType.CHARGE, 200L)
+		val usePointDto = PointDto(1L, TransactionType.USE, 100L)
+
+		sut.chargeUserPoint(chargePointDto)
+		sut.useUserPoint(usePointDto)
+
+		val actual = sut.getAllPointHistoriesByUserId(1L)
+
+		assertAll(
+			{ assertThat(actual.size).isEqualTo(2) },
+			{ assertThat(actual[1].userId).isEqualTo(1L) },
+			{ assertThat(actual[1].type).isEqualTo(TransactionType.USE) },
+			{ assertThat(actual[1].amount).isEqualTo(100L) },
+		)
+	}
+
+	@Test
+	@DisplayName("포인트 사용 중 잔고 부족으로 실패 예외케이스")
+	fun shouldExceptionLeakUsePointTest() {
+		val pointDto = PointDto(1L, TransactionType.USE, 100L)
+
+		assertThrows<PointException> {
+			sut.useUserPoint(pointDto)
 		}
 	}
 }
