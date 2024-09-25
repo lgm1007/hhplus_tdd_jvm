62c20a44ab8097f55892db5ec6ccd9c0a8771a91
lgm1007 <flxl654@gmail.com>
2024-09-25 21:43:44 +0900

    test: 포인트 충전/사용 동작에 대한 병렬 요청 동시성 제어, 동일한 사용자에 대한 요청은 순차적으로 수행하는지 테스트

--- src/test/kotlin/io/hhplus/tdd/point/service/PointServiceConcurrentTest.kt
+++ src/test/kotlin/io/hhplus/tdd/point/service/PointServiceConcurrentTest.kt
@@ -71,4 +71,82 @@ class PointServiceConcurrentTest {
 			executor.shutdown()
 		}
 	}
+
+	@Test
+	@DisplayName("포인트 충전 및 사용의 병렬 요청에 대한 동시성 제어 기능 테스트")
+	fun handleChargeAndUsePointConcurrentTest() {
+		// 최대 10개의 스레드를 가진 스레드 풀 생성
+		val executor = Executors.newFixedThreadPool(10)
+
+		try {
+			// 아이디 1L인 유저의 포인트 충전 & 사용 요청
+			// 순서: 700 충전 -> 300 사용 -> 400 사용 -> 500 충전 -> 200 사용
+			val user1Future = CompletableFuture.completedFuture(Unit)
+				.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.chargeUserPoint(PointDto(1L, TransactionType.CHARGE, 700L))
+					}, executor)
+				}.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.useUserPoint(PointDto(1L, TransactionType.USE, 300L))
+					}, executor)
+				}.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.useUserPoint(PointDto(1L, TransactionType.USE, 400L))
+					}, executor)
+				}.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.chargeUserPoint(PointDto(1L, TransactionType.CHARGE, 500L))
+					}, executor)
+				}.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.useUserPoint(PointDto(1L, TransactionType.USE, 200L))
+					}, executor)
+				}
+
+			// 아이디 2L인 유저의 포인트 충전 & 사용 요청
+			// 순서: 500 충전 -> 300 사용 -> 200 사용 -> 600 충전 -> 400 사용
+			val user2Future = CompletableFuture.completedFuture(Unit)
+				.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.chargeUserPoint(PointDto(2L, TransactionType.CHARGE, 500L))
+					}, executor)
+				}.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.useUserPoint(PointDto(2L, TransactionType.USE, 300L))
+					}, executor)
+				}.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.useUserPoint(PointDto(2L, TransactionType.USE, 200L))
+					}, executor)
+				}.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.chargeUserPoint(PointDto(2L, TransactionType.CHARGE, 600L))
+					}, executor)
+				}.thenCompose {
+					CompletableFuture.supplyAsync({
+						sut.useUserPoint(PointDto(2L, TransactionType.USE, 400L))
+					}, executor)
+				}
+
+			// 모든 작업이 완료될 때까지 대기
+			user1Future.join()
+			user2Future.join()
+
+			val user1Point = userPointTable.selectById(1L).point
+			val user2Point = userPointTable.selectById(2L).point
+
+			val user1Histories = pointHistoryTable.selectAllByUserId(1L)
+			val user2Histories = pointHistoryTable.selectAllByUserId(2L)
+
+			assertAll(
+				{ assertThat(user1Point).isEqualTo(300L) },
+				{ assertThat(user2Point).isEqualTo(200L) },
+				{ assertThat(user1Histories.size).isEqualTo(5) },
+				{ assertThat(user2Histories.size).isEqualTo(5) }
+			)
+		} finally {
+			executor.shutdown()
+		}
+	}
 }
